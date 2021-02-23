0752320f54b3e135b0642356587e92354cc6f555
electrosalaf <51977981+electrosalaf@users.noreply.github.com>
2021-02-23 10:54:01 +0100

    Add click listener on list item to open website URL

---
+++ app/src/main/java/com/example/quakereport/Earthquake.java
@@ -0,0 +1,18 @@
+package com.example.quakereport;
+
+public class Earthquake {
+
+    /**
+     * An {@link Earthquake} object contains information related to a single earthquake.
+     */
+
+    /** Magnitude of the earthquake */
+    private double mMagnitude;
+
+    /** Location of the earthquake */
+    private String mLocation;
+
+    /** Time of the earthquake */
+    private long mTimeInMilliSeconds;
+
+}
